d91e7211481b3a0174df932c4491fda96cf41161
camchoo <70016523+gamcho3@users.noreply.github.com>
2021-02-06 22:24:17 +0900

    edit kokoa clone

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const password = document.getElementById('password');
+const email = document.getElementById('email');
+
+function checkpd() {
+
+}
+
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    checkpd
+
+})
